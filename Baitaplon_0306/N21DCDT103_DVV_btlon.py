1d8e6d5c336746a71c7ca2cd730e9167a969d5be
DangVanVu-ctdl <n21dcdt103@student.ptithcm.edu.vn>
2024-06-03 07:40:37 +0700

    hello w

--- Baitaplon_0306/N21DCDT103_DVV_btlon.py
+++ Baitaplon_0306/N21DCDT103_DVV_btlon.py
@@ -1,7 +1,1 @@
-def getFirstLastName (name):
-    return [list[2],list[0]]
-name = "Dang Van Vu"
-result = getFirstLastName(name)
-result[0] = "unknown"
-result[1] = "unknown"
-print(result)
+print("hello world")
